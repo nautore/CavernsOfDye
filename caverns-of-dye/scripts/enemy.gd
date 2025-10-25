6a7ea0a4a1b6e7c61b2bea65d8b74443eec18aaa
nautore <nautore@proton.me>
2025-10-25 11:43:29 -0700

    Implemented basic saving/loading system to save player position and number of insects captured. Save/Load utilizes Godot Resource to store save data into a .tres file (text) which will then be converted to a binary .res file upon game export. The ResourceSaver singleton is used to save the Resource to the filesystem. Also reorganized node structure and remade UI.

--- caverns-of-dye/scripts/enemy.gd
+++ caverns-of-dye/scripts/enemy.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var SPEED = 40
 var health = 100
-@onready var player = $/root/GameWorld/Player
+@onready var player = %Player
 
 func _physics_process(delta: float) -> void:
 	velocity = (player.global_position - global_position).normalized() * SPEED
