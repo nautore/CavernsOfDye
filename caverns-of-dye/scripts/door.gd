e3fa3a432825ae1d51041f643163830925dea7b6
nautore <nautore@proton.me>
2025-11-25 14:59:48 -0800

    Expanded some systems related to exploring the cave Enemies/insects will no longer respawn after leaving a cave level but this resets when entering safe zone More cave levels Music now loops Music will no longer restart when going between cave levels Added delay when changing levels to prevent reentering doors during transition

--- caverns-of-dye/scripts/door.gd
+++ caverns-of-dye/scripts/door.gd
@@ -1,10 +1,18 @@
 extends Area2D
 
+@export var inCave:bool = false
 @export var next_level:String
+@export var next_player_x:int
+@export var next_player_y:int
 @export var locked := false
 var enemies
+var can_transition = false
 
 func _ready() -> void:
+	# prevent multiple teleportations (needed here too)
+	var transition_timer = get_tree().create_timer(0.5)
+	transition_timer.connect("timeout", Callable(self, "_on_transition_timer_timeout"))
+	
 	enemies = get_tree().get_nodes_in_group("enemies")
 	if(enemies.size() != 0):
 		locked = true
@@ -14,9 +22,23 @@ func _on_body_entered(body: Node2D) -> void:
 		if(enemies.size() == 0):
 			locked = false
 		#Global.game.change_scene(Global.levels.get(next_level))
-		if(!locked):
-			Global.game.call_deferred("change_scene", Global.levels.get(next_level))
+		if(!locked && can_transition):
+			#Global.game.call_deferred("change_scene", Global.levels.get(next_level))
+			
+			# prevent multiple teleportations
+			can_transition = false
+			var transition_timer = get_tree().create_timer(0.5)
+			transition_timer.connect("timeout", Callable(self, "_on_transition_timer_timeout"))
+			
+			# move player and change level
+			body.global_position.x = next_player_x
+			body.global_position.y = next_player_y
+			Global.game.call_deferred("change_scene", next_level)
 			if(next_level == "safe_zone"):
 				GlobalAudio.play_music("background_music")
 			else:
-				GlobalAudio.play_music("cave_music")
+				# only start cave music when entering cave for the first time
+				if(!inCave):
+					GlobalAudio.play_music("cave_music")
+func _on_transition_timer_timeout() -> void:
+	can_transition = true
--- caverns-of-dye/scripts/game.gd
+++ caverns-of-dye/scripts/game.gd
@@ -9,13 +9,34 @@ func _ready() -> void:
 	Global.game = self
 	#current_scene = load("res://scenes/levels/safe_zone.tscn").instantiate()
 	#world_root.add_child(current_scene)
-	change_scene(Global.levels.get("safe_zone"))
+	
+	#change_scene(Global.levels.get("safe_zone"))
+	change_scene("safe_zone")
 	
 func change_scene(new_scene: String):
 	if(current_scene != null):
 		current_scene.queue_free()
-	var new = load(new_scene).instantiate()
+	#var new = load(new_scene).instantiate()
+	var scene_path = Global.levels.get(new_scene)
+	var new = load(scene_path).instantiate()
 	world_root.add_child(new)
 	current_scene = new
+	
 	player.connect_insects()
 	
+	# reset enteredLevels when entering safe_zone
+	if(new_scene == "safe_zone"):
+		for key in Global.enteredLevels:
+			Global.enteredLevels.set(key, false)
+		return
+	
+	# cleared already entered levels of enemies and insects
+	if(Global.enteredLevels.get(new_scene) == true):
+		var enemies = get_tree().get_nodes_in_group("enemies")
+		var insects = get_tree().get_nodes_in_group("insects")
+		for enemy in enemies:
+			enemy.queue_free()
+		for insect in insects:
+			insect.queue_free()
+	else:
+		Global.enteredLevels.set(new_scene, true)
